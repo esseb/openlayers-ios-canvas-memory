e5e43e125b0ac34c4616854c90d61ff6ba7aacb9
Stein Cato Blostrupmoen <esseb@esseb.com>
2019-03-06 14:47:27 +0100

    Reduce number of feature layers

--- source/index.js
+++ source/index.js
@@ -36,7 +36,7 @@ function createMap() {
   // Add several vector layers with one feature each.
   // This is a bit excessive, but demonstrates the problem.
   vectorLayers = [];
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < 6; i++) {
     const vectorLayer = new VectorLayer({
       source: new VectorSource(),
       style: new Style({
